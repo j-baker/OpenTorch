1c6f7200d13e28246cc46fb7fda7769a5c3fbb7c
James Baker <j.baker@outlook.com>
2014-05-09 02:08:29 +0100

    App functional in loose sense.

--- OpenTorch/src/main/java/uk/ac/cam/jeb200/opentorch/TorchControl.java
+++ OpenTorch/src/main/java/uk/ac/cam/jeb200/opentorch/TorchControl.java
@@ -1,17 +1,116 @@
 package uk.ac.cam.jeb200.opentorch;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
 
 
 public class TorchControl extends ActionBarActivity {
+    private Camera camera = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_torch_control);
+    }
+
+    private SurfaceHolder getSurfaceHolder() {
+        final SurfaceView preview = (SurfaceView)findViewById(R.id.PREVIEW);
+        return preview.getHolder();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final ToggleButton button = (ToggleButton)findViewById(R.id.lightSwitch);
+        button.setChecked(false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    private Camera getCamera() {
+        releaseCamera();
+        if (deviceHasFlash(this)) {
+            try {
+                camera = Camera.open();
+            } catch (RuntimeException e) {
+                camera = null;
+            }
+        } else {
+            camera = null;
+        }
+        return camera;
+    }
+
+    public void onToggleClicked(View view) {
+        final boolean on = ((ToggleButton) view).isChecked();
+
+        if (on) {
+            torchOn();
+        } else {
+            torchOff();
+        }
+    }
+
+    private boolean deviceHasFlash(Context context) {
+        final PackageManager p = context.getPackageManager();
+        return p != null && p.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+        }
+        camera = null;
+        Log.i(getString(R.string.app_name), getString(R.string.debug_light_off));
+    }
+
+    private void setFlashMode(Camera cam) {
+        final Parameters p = cam.getParameters();
+        p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+        cam.setParameters(p);
+    }
+
+    private void setPreviewDisplay(Camera cam) {
+        try {
+            cam.setPreviewDisplay(getSurfaceHolder());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void torchOn() {
+        final Camera cam = getCamera();
+        if (cam != null) {
+            setFlashMode(cam);
+            setPreviewDisplay(cam);
+            cam.startPreview();
+            Log.i(getString(R.string.app_name), getString(R.string.debug_light_on));
+        } else {
+            Log.w(getString(R.string.app_name), getString(R.string.debug_camera_error));
+        }
+    }
+
+    private void torchOff() {
+        releaseCamera();
     }
 
 
@@ -34,5 +133,4 @@ public class TorchControl extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
