2a6fe55fbcaf7dffb2ffbc63c12e88c800dd8916
James Baker <j.baker@outlook.com>
2014-05-09 02:44:26 +0100

    Removed unnecessary menu.

--- OpenTorch/src/main/java/uk/ac/cam/jeb200/opentorch/TorchControl.java
+++ OpenTorch/src/main/java/uk/ac/cam/jeb200/opentorch/TorchControl.java
@@ -27,6 +27,7 @@
 
 package uk.ac.cam.jeb200.opentorch;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
@@ -44,7 +45,7 @@ import android.widget.ToggleButton;
 import java.io.IOException;
 
 
-public class TorchControl extends ActionBarActivity {
+public class TorchControl extends Activity {
     private Camera camera = null;
 
     @Override
@@ -138,26 +139,5 @@ public class TorchControl extends ActionBarActivity {
 
     private void torchOff() {
         releaseCamera();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.torch_control, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
